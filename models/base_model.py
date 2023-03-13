79ec737aa99ab05c83ad197a7433bafbc58bc3e0
girmesh03 <girmachewzewdie03@gmail.com>
2023-03-14 00:46:34 +0300

    task 3 update 2

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,18 @@ class BaseModel:
     """This class will define all common
     attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiation of base model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -27,6 +37,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values
         of __dict__ of the instance"""
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
